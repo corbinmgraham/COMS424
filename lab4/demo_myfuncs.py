8b8dd5407ebd3e8c06d537e4f0cdbbec02c1e747
Corbin Graham <corbin@metagraham.com>
2023-09-05 12:57:43 -0400

    Added lab 5

--- lab4/demo_myfuncs.py
+++ lab4/demo_myfuncs.py
@@ -1,2 +1,35 @@
 import myfuncs as mf
+import math
+import numpy as np
 
+if __name__ == "__main__":
+    # Factorial Test
+    print(f"Factorial\n Num | {'Math':<15} | {'Mine':<15} | Equal")
+    for i in range(20):
+        mm = math.factorial(i)
+        mn = mf.factorial(i)
+        print(f"{i:4} | {mm:>15d} | {mn:<15d} | {(mm == mn)}")
+
+    # Square Root Test
+    print(f"Square Root\n Num | {'Math':<15} | {'Mine':<15} | Equal | Difference")
+    for i in range(18):
+        mm = math.sqrt(i)
+        mn = mf.sqrt(i)
+        diff = (abs((mm - mn)) / ((mm + mn) / 2)) * 100
+        print(f"{i:4} | {mm:.13f} | {mn:.13f} | {(mm == mn)} | {diff}")
+
+    # Exp Test
+    print(f"Exp\n Num | {'Math':<15} | {'Mine':<15} | Equal | Difference")
+    for i in range(18):
+        mm = math.exp(i)
+        mn = mf.exp(i)
+        diff = (abs((mm - mn)) / ((mm + mn) / 2)) * 100
+        print(f"{i:4} | {mm:.13f} | {mn:.13f} | {(mm == mn)} | {diff}")
+
+    # Ln Test
+    print(f"Ln\n Num | {'Numpy':<15} | {'Mine':<15} | Equal | Difference")
+    for i in range(18):
+        mm = np.log(i)
+        mn = mf.ln(i)
+        diff = (abs((mm - mn)) / ((mm + mn) / 2)) * 100
+        print(f"{i:4} | {mm:.13f} | {mn:.13f} | {(mm == mn)} | {diff}")
--- lab4/myfuncs.py
+++ lab4/myfuncs.py
@@ -1,43 +1,66 @@
-def factorial(x):
+def factorial(x,log=False):
+    if log == True:
+        print(f"{x}!")
     if x < 2:
+        if log == True:
+            print("The input is less nonpositive")
         return 1
-    return (x * factorial(x-1))
+    return (x * factorial((x-1), log))
+
 def sqrt(x):
     s = 1.0
     for k in range(100):
         s = 0.5 * (s + x/s)
     return s
 
-def exp(x):
-    import math
-    e = 2.7182818284509451 # Stored value for e
+def exp(x,log=False,kmax=100,tol=1.0e-14):
+    e_const = 2.7182818284509451 # Stored value for e
     x0 = int(round(x))
-    z = x-x0
-    ex0 = e**x0
+    z = 1.0*(x-x0)
+    ex0 = e_const**x0
     
     s = 0.0
-    for k in range(20):
-        s = s + z**k / math.factorial(k)
+    for k in range(kmax):
+        sold = z**k / factorial(k)
+        s = s + sold
+
+        if log == True:
+            print(f"S: {s} | New: {sold}")
+        if (abs(sold) < tol):
+            break
     return ex0 * s
 
-def ln(x):
-    s = x*10
-    for k in range(100):
+def ln(x,log=False,kmax=100,tol=1.0e-14):
+    s = x*10.0
+    for k in range(kmax):
         s = s - 1 + x * exp(-s)
+        if (log == True):
+            print(f"S: {s}")
+        if (s < tol):
+            break
     return s
 
-if __name__ == "__main__":
-    # Factorial Test
-    import math
-    print(f"Factorial\n Num | {'Math':<15} | {'Mine':<15} | Equal")
-    for i in range(20):
-        mm = math.factorial(i)
-        mn = factorial(i)
-        print(f"{i:4} | {mm:>15d} | {mn:<15d} | {(mm == mn)}")
+def sqrt_newton(x, log=False, initial_guess=1.0, kmax=100, tol=1.0e-14):
+    """
+    Square Root Function using Newton's Method
+    """
 
-    # Factorial Test
-    print(f"Square Root\n Num | {'Math':<15} | {'Mine':<15} | Equal")
-    for i in range(18):
-        mm = math.sqrt(i)
-        mn = sqrt(i)
-        print(f"{i:4} | {mm:.15} | {mn:.15} | {(mm == mn)}")
+    # Convert x to float
+    x = 1.0 * x
+
+    if x <= 0:
+        print("Input cannot be negative or 0")
+
+    s = initial_guess
+    for k in range(kmax):
+        if log == True:
+            print(f"S: {s} | K: {k}")
+        
+        sold = s
+        s = 0.5 * (s + x/s)
+        if (abs((s-sold) / x) < tol):
+            break
+
+    if log == True:
+        print(f"Final Result: {s}")
+    return s
